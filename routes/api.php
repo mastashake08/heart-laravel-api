243d0941a39805a756d99ebdbc3721dfc576b7d1
Jyrone Parker <jyrone.parker@mgmail.com>
2021-05-12 10:26:46 -0400

    added SMS responses

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use Twilio\TwiML\MessagingResponse;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/sms', function(Request $request) {
+  header("content-type: text/xml");
+
+  $response = new MessagingResponse();
+  $response->message(
+      "Welcome to HEART!"
+  );
+
+  echo $response;
 });
