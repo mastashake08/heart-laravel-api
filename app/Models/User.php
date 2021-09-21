c21ef41a9986e160e1261a4d03952f8c2fbfb9ef
Jyrone Parker <jyrone.parker@mgmail.com>
2021-09-21 11:04:09 -0400

    remove verifyemail for demo

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
 use Laravel\Cashier\Billable;
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable //implements MustVerifyEmail
 {
     use HasApiTokens;
     use HasFactory;
@@ -25,7 +25,7 @@ class User extends Authenticatable implements MustVerifyEmail
      * @var array
      */
     protected $guarded = [
-      
+
     ];
 
     /**
