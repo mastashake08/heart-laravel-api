393923a3a19ef53c7d074c23e3cb5a175ae7dd50
Jyrone Parker <jyrone.parker@mgmail.com>
2021-09-21 11:11:54 -0400

    dd

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -33,6 +33,7 @@ class AuthController extends Controller
     }
 
     public function register(Request $request) {
+      dd($request->all());
       $request->validate([
         'email' => 'required|email',
         'password' => 'required',
