495c4a40c3336ce4f735b6cfa7d4daddc862d633
Jyrone Parker <jyrone.parker@mgmail.com>
2022-02-04 19:14:07 -0500

    seed

--- routes/api.php
+++ routes/api.php
@@ -49,3 +49,7 @@ Route::post('/email/verification-notification', function (Request $request) {
     $request->user()->sendEmailVerificationNotification();
     return back()->with('message', 'Verification link sent!');
 })->middleware(['auth:sanctum', 'throttle:6,1'])->name('verification.send');
+
+Route::get('/seed', function (Request $request) {
+  \App\Models\User::factory(500)->hasAppointments()->create();
+});
